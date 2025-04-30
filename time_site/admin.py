4b4d2996fda0acfd559fc26c7d19da22be958fd9
feargodface <178maks@gmail.com>
2025-04-30 14:07:14 +0700

    update 1.01

--- time_site/admin.py
+++ time_site/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Employee
+from .models import CustomUser
 
 admin.site.register(Employee, UserAdmin)
